a45abe364415322f2ded912f54e736fd1f75aab8
abhishek <alokyadav383@gmail.com>
2021-09-23 14:59:50 +0530

    APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView (APIView):
+    def get(self,request,format=None):
+        an_apiview=[
+            'Uses HTTP method '
+            'Is similer to Django'
+            'Hey '
+            'How are you'
+        ]
+        return Response({'message':'Hello !','an_apiview':an_apiview})
